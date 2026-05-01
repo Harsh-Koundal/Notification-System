5945577499684a798c34e68c0610eedfd28adddc
Harsh-Koundal <aharsh3039@gmail.com>
2026-05-01 14:13:41 -0400

    kafkaConsumer for storing the notification in DB

--- Server-Side/kafka/kafkaConsumer.js
+++ Server-Side/kafka/kafkaConsumer.js
@@ -1,5 +1,13 @@
 import { Kafka } from "kafkajs";
 import { sendMail } from "../utils/mailer.js";
+import Notification from "../model/Notification.js";
+import { connectDB } from "../config/db.js";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+    path:path.resolve("../.env"),
+});
 
 
 const kafka = new Kafka({
@@ -10,19 +18,34 @@ const kafka = new Kafka({
 const consumer = kafka.consumer({groupId:"email-group"});
 
 await consumer.connect();
+await connectDB();
 
-await consumer.subscribe({topic:"user-events"});
+await consumer.subscribe({
+    topic:"user-events",
+    fromBeginning:true,
+});
 
 await consumer.run({
     eachMessage: async({message})=>{
         const event = JSON.parse(message.value.toString());
 
         if(event.type === "user.signup"){
-            await sendMail({
-                to:event.data.email,
-                subject:"Welcome",
-                text:"Thankyou for signup"
-            })
+            try{
+                await Notification.create({
+                    userId:event.data.userId,
+                    type:"EMAIL",
+                    eventType:"user.signup",
+                    title:"Welcome",
+                    message:`Hi ${event.data.name}, thanks for signing up!`,
+                    status:"PENDING",
+                    metadata:{
+                        email:event.data.email,
+                    },
+                });
+                console.log("Notification stored in DB");
+            }catch(err){
+                console.error("Failed to store Notification",err);
+            }
         }
-    }
+    },
 });
