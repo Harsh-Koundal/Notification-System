ac5aace296a8d715284771d8eb84f1866a13698f
Harsh-Koundal <aharsh3039@gmail.com>
2026-05-02 11:14:38 -0400

    locking system

--- Server-Side/workers/emailWorker.js
+++ Server-Side/workers/emailWorker.js
@@ -6,62 +6,72 @@ import { sendMail } from "../utils/mailer.js";
 import { getBackoffTime } from "../utils/backoff.js";
 
 dotenv.config({
-    path:path.resolve("../.env")
+    path: path.resolve("../.env")
 })
 
 const MAX_RETRY = 3;
 
-const processEmail = async()=>{
+const processEmail = async () => {
     await connectDB();
     console.log("Email Worker Running...");
 
-    while(true){
-        try{
-            const notifications = await Notification.find({
-                status:"PENDING",
-                type:"EMAIL",
-                $or:[
-                    {scheduledAt:null},
-                    {scheduledAt:{$lte:new Date()}}
+    while (true) {
+        try {
+            const notif = await Notification.findOneAndUpdate({
+                status: "PENDING",
+                type: "EMAIL",
+                $or: [
+                    { scheduledAt: null },
+                    { scheduledAt: { $lte: new Date() } }
                 ]
-            }).limit(5);
-
-            for(const notif of notifications){
-                try{
-                    await sendMail({
-                        to:notif.metadata.email,
-                        subject:notif.title,
-                        text:notif.message,
-                    });
-
-                    notif.status = "SENT";
-                    notif.sentAt = new Date();
-                    await notif.save();
-
-                    console.log(`Email sent to ${notif.metadata.email}`);
-                }catch(err){
-                    notif.retryCount += 1;
-                    notif.error = err.message;
-
-                    if(notif.retryCount >= MAX_RETRY){
-                        notif.status = "FAILED",
-                        console.log(`Permanently Failed for ${notif.metadata.email}`);
-                    }else{
-                        const delay = getBackoffTime(notif.retryCount);
-
-                        notif.status = "PENDING";
-                        notif.scheduledAt = new Date(Date.now() + delay);
-                        console.warn(`Retry ${notif.retryCount} in ${delay / 1000}s for ${notif.metadata.email}`);
-                    }
-
-                    await notif.save();
-                    console.log("Email Failed",err.message);
+            },
+                {
+                    $set: { status: "PROCESSING" }
+                },
+                {
+                    new: true
                 }
+            );
+
+            if(!notif){
+                await new Promise(res=> setTimeout(res,2000));
+                continue;
             }
-        }catch(err){
-            console.error("Worker Error",err);
+
+            try {
+                await sendMail({
+                    to: notif.metadata.email,
+                    subject: notif.title,
+                    text: notif.message,
+                });
+
+                notif.status = "SENT";
+                notif.sentAt = new Date();
+                await notif.save();
+
+                console.log(`Email sent to ${notif.metadata.email}`);
+            } catch (err) {
+                notif.retryCount += 1;
+                notif.error = err.message;
+
+                if (notif.retryCount >= MAX_RETRY) {
+                    notif.status = "FAILED",
+                    console.log(`Permanently Failed for ${notif.metadata.email}`);
+                } else {
+                    const delay = getBackoffTime(notif.retryCount);
+
+                    notif.status = "PENDING";
+                    notif.scheduledAt = new Date(Date.now() + delay);
+                    console.warn(`Retry ${notif.retryCount} in ${delay / 1000}s for ${notif.metadata.email}`);
+                }
+
+                await notif.save();
+                console.log("Email Failed", err.message);
+            }
+
+        } catch (err) {
+            console.error("Worker Error", err);
         }
-        await new Promise(res=>setTimeout(res,2000));
     }
 };
 processEmail();
